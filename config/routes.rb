3f7be2f27cee5fb16cc394120d7136affcd1157f
yuken02 <git@github.com:yuken02/practice.git>
2023-01-09 16:08:19 +0000

    [Add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root to: 'homes/top'
   get 'trends/index'
-  get 'homes/top'
+  resources :searchs, only: [:index, :update, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
