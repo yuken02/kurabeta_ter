2559c085e5908029ed5ea90f6cb8dd64a4d0bb63
yuken02 <git@github.com:yuken02/practice.git>
2023-01-09 16:58:20 +0000

    [Add]user(devise) controller追加,routing追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+   registrations: 'users/registrations',
+   sessions:      'users/sessions',
+}
 
-  root to: 'homes/top'
-  get 'trends/index'
+  root to: 'homes#top'
+  resources :trends, only: [:index]
   resources :searchs, only: [:index, :update, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
