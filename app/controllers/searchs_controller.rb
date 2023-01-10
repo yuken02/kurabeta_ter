757d81e29000314622aa389ccd20969dbb2e2874
yuken02 <git@github.com:yuken02/practice.git>
2023-01-10 14:22:26 +0000

    [Add]trendページ、検索ページ

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,2 +1,22 @@
 class SearchsController < ApplicationController
+
+  def index
+    require 'json'
+    require 'uri'
+    require 'net/http'
+
+    keyword = URI.encode_www_form_component(params[:keyword])
+    bearer_token = ENV['BEARER_TOKEN']
+    uri = URI.parse("https://api.twitter.com/2/tweets/counts/recent?query=#{keyword}&granularity=hour")
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req["Authorization"] = "bearer #{bearer_token}"
+
+    res = http.request(req)
+    @tweets = res.body
+  end
 end
--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,4 +1,24 @@
 class TrendsController < ApplicationController
   def index
+    client = Twitter::REST::Client.new do |config|
+      # 事前準備で取得したキーのセット
+      config.consumer_key = ENV['API_KEY']
+      config.consumer_secret = ENV['API_KEY_SECRET']
+      config.access_token = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
+
+    @i = 0
+    @trends = client.trends(id = 23424856).attrs[:trends].first(30)
+    # @trends = get_trends.each do |trend|
+    #     name = trend[:name]
+    #     url = trend[:url]
+    #     promoted_content = trend[:promoted_content]
+    #     query = trend[:query]
+    #     tweet_volume = trend[:tweet_volume]
+    #     # puts "#{name}, #{url}, #{promoted_content}, #{query}, #{tweet_volume}"
+    #   end
+
+    # @trends = JSON.parse(get_trends)
   end
 end
