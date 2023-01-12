bfddae09ed4f5e1d0c12406cfd598e5aececa45b
yuken02 <git@github.com:yuken02/practice.git>
2023-01-12 08:58:58 +0000

    [Add]ゲストログイン

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   resources :trends, only: [:index]
   resources :searchs, only: [:index, :update, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
