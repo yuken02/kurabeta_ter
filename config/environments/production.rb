b2e686c6decce809765a911e5c224ae0e613dcb7
yuken02 <git@github.com:yuken02/practice.git>
2023-01-24 12:33:42 +0000

    [Fix]パスワード再設定機能

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "kurabeta_ter_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'example.com'}
+  config.action_mailer.default_url_options = { host: 'http://52.194.109.155/' }
+  config.action_mailer.perform_caching = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV["GOOGLE_MAIL_ADDRESS"],
+    :password => ENV["GOOGLE_MAILER_PASSWORD"],
+    :authentication => 'login'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
