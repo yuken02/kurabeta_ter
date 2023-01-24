7759eba8b4e808c768805864b62b30bc74dad170
yuken02 <git@github.com:yuken02/practice.git>
2023-01-24 11:21:20 +0000

    [Update]メール文の日本語化

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   #追記
-  config.action_mailer.default_url_options = { host: 'http://52.194.109.155' }
+  config.action_mailer.default_url_options = { host: 'http://52.194.109.155/' }
   config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
