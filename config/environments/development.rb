1be4f2d9bbd4fa6d39ef8d69f290f81acd11939c
yuken02 <git@github.com:yuken02/practice.git>
2023-01-24 10:50:32 +0000

    [Fix]パスワード再設定機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,20 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  #追記
+  config.action_mailer.default_url_options = { host: 'http://52.194.109.155' }
+  config.action_mailer.perform_caching = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV["GOOGLE_MAIL_ADDRESS"],
+    :password => ENV["GOOGLE_MAILER_PASSWORD"],
+    :authentication => 'login'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -224,7 +224,12 @@ Devise.setup do |config|
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
   # change their passwords.
-  config.reset_password_within = 6.hours
+  config.reset_password_within = 2.hours
+  config.mailer_sender = ENV["GOOGLE_MAIL_ADDRESS"]
+  # パスワード再設定するためのキーカラム。
+  config.reset_password_keys = [:email]
+  #既定値はtrueで, リセットされた後に自動的にサインインする。
+  config.sign_in_after_reset_password = true
 
   # When set to false, does not sign a user in automatically after their password is
   # reset. Defaults to true, so a user is signed in automatically after a reset.
