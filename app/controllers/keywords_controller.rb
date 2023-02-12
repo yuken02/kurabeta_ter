e285f6d7f4135699f5bdeb9e32244682db1839f8
yuken02 <git@github.com:yuken02/practice.git>
2023-02-12 19:13:30 +0000

    [Update]編集モーダル 非同期通信未完成

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -7,7 +7,7 @@ class KeywordsController < ApplicationController
     if word.count < 30
       @word_new = Keyword.new(word_params)
       if @word_new.save
-        redirect_to search_path, notice: "キーワードを登録しました"
+        # redirect_to search_path, notice: "キーワードを登録しました"
       else
         @keyword = params[:keyword]
         redirect_to request.referer, alert: "キーワードの登録に失敗しました"
