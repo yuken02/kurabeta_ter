802005204a1e68ac5b71ba150c38ea5f228ca64e
yuken02 <git@github.com:yuken02/practice.git>
2023-02-13 12:18:59 +0000

    [Update]編集モーダル・sideber 非同期通信

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -4,8 +4,8 @@ class KeywordsController < ApplicationController
   def create
     tabs = Tab.where(user_id: current_user.id)
     word = Keyword.where(tab_id: tabs.ids)
+    @word_new = Keyword.new(word_params)
     if word.count < 30
-      @word_new = Keyword.new(word_params)
       if @word_new.save
         # redirect_to search_path, notice: "キーワードを登録しました"
       else
@@ -13,11 +13,19 @@ class KeywordsController < ApplicationController
         redirect_to request.referer, alert: "キーワードの登録に失敗しました"
       end
     end
+    @tabs = Tab.where(user_id: current_user.id)
+    @tabs_count = @tabs.count
+    @tab_new = Tab.new
+    @word = Keyword.where(tab_id: @tabs.ids)
   end
 
   def destroy
-    @word = Keyword.find(params[:id])
-    @word.destroy
+    word = Keyword.find(params[:id])
+    word.destroy
+    @tabs = Tab.where(user_id: current_user.id)
+    @tabs_count = @tabs.count
+    @tab_new = Tab.new
+    @word = Keyword.where(tab_id: @tabs.ids)
     # redirect_to request.referer, notice: "キーワードを削除しました"
   end
 
